a8e38cb1d09591120e7091bbbcfbd6f717ac7a18
tabascum <marques_vasco@yahoo.es>
2022-07-25 07:11:59 +0100

    upload project files

---
+++ newapp.js
@@ -0,0 +1,21 @@
+/* function to animate text */
+
+let i = -1;
+let txt = 'FrontEnd Web Developer'; /* The text */
+let speed = 110; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("letters").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+typeWriter();
+
+
+  /* modals */
+
+
+
